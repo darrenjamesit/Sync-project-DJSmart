3f0184618d6ead9d4472f0d83519f894fdf5bd02
Darren James <darrenjames.it@gmail.com>
2023-07-07 19:45:52 +0300

    interval variable

--- sync-project.py
+++ sync-project.py
@@ -3,6 +3,7 @@ import time
 
 src = input("please enter source directory...")
 rep = input("please enter replica directory...")
+sync_int = int(input("please enter a synchronization interval (in seconds)..."))
 
 def one_way_sync(src, rep):
     run = True
