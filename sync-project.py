f2dce355bf3a34b0dbbb8178eba940f38a7d8a91
Darren James <darrenjames.it@gmail.com>
2023-07-07 19:49:47 +0300

    calling function

--- sync-project.py
+++ sync-project.py
@@ -1,14 +1,16 @@
 import os
 import time
 
-src = input("please enter source directory...")
-rep = input("please enter replica directory...")
-sync_int = int(input("please enter a synchronization interval (in seconds)..."))
+src = input("please enter source directory... ")
+rep = input("please enter replica directory... ")
+sync_int = int(input("please enter a synchronization interval (in seconds)... "))
 
-def one_way_sync(src, rep):
+def one_way_sync(source_folder: str, replica_folder: str):
     run = True
     while run:
         print(src)
         print(rep)
         break
 
+
+one_way_sync(src, rep)
