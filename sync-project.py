197d43eae599e56a50931861deac73ba76ae4bbd
Darren James <darrenjames.it@gmail.com>
2023-07-07 19:44:35 +0300

    setup of function

--- sync-project.py
+++ sync-project.py
@@ -1,3 +1,13 @@
 import os
 import time
 
+src = input("please enter source directory...")
+rep = input("please enter replica directory...")
+
+def one_way_sync(src, rep):
+    run = True
+    while run:
+        print(src)
+        print(rep)
+        break
+
