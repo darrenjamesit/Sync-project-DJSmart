1f111450811a1a0e2ceade57669afa3bfd203052
Darren James <darrenjames.it@gmail.com>
2023-07-11 14:07:44 +0300

    created a simple continue / stop if statement

--- sync-project.py
+++ sync-project.py
@@ -7,10 +7,23 @@ sync_int = int(input("please enter a synchronization interval (in seconds)... ")
 
 def one_way_sync(source_folder: str, replica_folder: str):
     run = True
+    i = 0
     while run:
         print(src)
         print(rep)
-        break
+
+        # counter to ask if synchronisation should continue every 5 loops
+        i += 1
+        if i == 5:
+            run_query = input("would you like to continue? y / n")
+            if run_query == 'y':
+                run = True
+                i = 0
+            elif run_query == 'n':
+                run = False
+            else:
+                run = True
+                print("Invalid response, please type only y or n, cycle will continue.")
 
 
 one_way_sync(src, rep)
